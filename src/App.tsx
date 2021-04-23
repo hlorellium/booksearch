be9b8aa4fe495746f86a84f7c523751d3c6e6a14
Pete <petr200118@gmail.com>
2021-04-23 18:32:33 +0300

    added search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Books from './features/books/Books';
+import SearchBar from './features/searchBar/SearchBar';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <SearchBar />
                 <Books />
             </header>
         </div>
---
+++ src/features/searchBar/SearchBar.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useAppDispatch } from '../../app/hooks';
+import { loadBooks } from '../books/booksSlice';
+
+function SearchBar() {
+    const [query, setQuery] = useState('');
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        const delayTimer = setTimeout(() => {
+            dispatch(loadBooks(query));
+        }, 1000);
+
+        return () => clearTimeout(delayTimer);
+    }, [query]);
+
+    return (
+        <div>
+            <div>
+                <span>Enter book's title</span>
+            </div>
+            <input
+                type="search"
+                name="booksSearch"
+                value={query}
+                onChange={(e) => {
+                    setQuery(e.target.value);
+                }}
+            />
+            <button type="submit" onClick={() => dispatch(loadBooks(query))}>
+                Search
+            </button>
+        </div>
+    );
+}
+
+export default SearchBar;

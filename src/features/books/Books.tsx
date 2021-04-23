f4f1d0f73112861187f5bb92be6cfa58439c8d31
Pete <petr200118@gmail.com>
2021-04-23 20:49:21 +0300

    removed button for mock request

--- src/features/books/Books.tsx
+++ src/features/books/Books.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import BookSnippet from './BookSnippet';
-import { selectBooks, loadBooks } from './booksSlice';
+import { selectBooks } from './booksSlice';
 
 function Books() {
     const books = useAppSelector(selectBooks);
-    const dispatch = useAppDispatch();
 
     return (
         <div>
-            <button
-                aria-label="Decrement value"
-                onClick={() => dispatch(loadBooks('harry potter'))}
-            >
-                +
-            </button>
             <ul>
                 {books.map((book, index) => (
                     <li key={index}>

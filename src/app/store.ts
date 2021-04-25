d7f121a32a728da7a5d661a8ce7d146f0f66e184
Pete <petr200118@gmail.com>
2021-04-25 14:07:05 +0300

    added loading spinner

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,7 +3,7 @@ import booksReducer from '../features/books/booksSlice';
 
 export const store = configureStore({
   reducer: {
-    books: booksReducer
+    books: booksReducer,
   },
 });
 
--- src/features/books/Books.tsx
+++ src/features/books/Books.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useAppSelector } from '../../app/hooks';
-import BookSnippet from './BookSnippet';
-import { selectBooks } from './booksSlice';
+import BookSnippet from './bookSnippet/BookSnippet';
+import { selectBooks, selectStatus } from './booksSlice';
 import styles from './Books.module.css';
+import LoadingSpinner from './loadingSpinner/LoadingSpinner';
 
 function Books() {
     const books = useAppSelector(selectBooks);
+    const status = useAppSelector(selectStatus);
 
     return (
         <div className={styles.container}>
+            {status === 'loading' ? <LoadingSpinner /> : null}
             <div className={styles.grid}>
                 {books.map((book, index) => (
                     <BookSnippet book={book} key={index} />
